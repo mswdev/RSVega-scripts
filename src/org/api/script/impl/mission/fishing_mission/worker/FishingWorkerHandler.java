4cef7731b2c2fc5015a6e357952cf06cdfd476b0
Matt <sphiinx33@gmail.com>
2019-03-01 17:02:11 -0700

    Updates to fishing

--- src/org/api/script/impl/mission/fishing_mission/worker/FishingWorkerHandler.java
+++ src/org/api/script/impl/mission/fishing_mission/worker/FishingWorkerHandler.java
@@ -1,22 +1,36 @@
 package org.api.script.impl.mission.fishing_mission.worker;
 
-import org.rspeer.runetek.adapter.component.Item;
-import org.rspeer.runetek.api.component.tab.Inventory;
 import org.api.game.skills.fishing.FishType;
 import org.api.script.framework.worker.Worker;
 import org.api.script.framework.worker.WorkerHandler;
 import org.api.script.impl.worker.MovementWorker;
 import org.api.script.impl.worker.banking.WithdrawWorker;
 import org.api.script.impl.worker.interactables.NpcWorker;
+import org.rspeer.runetek.adapter.component.Item;
+import org.rspeer.runetek.api.component.tab.Inventory;
 
 public class FishingWorkerHandler extends WorkerHandler {
+
+    private boolean temp_bool;
 
     @Override
     public Worker decide() {
         final FishType fish_type = FishType.SHRIMP;
-        final Item fish = Inventory.getFirst(fish_type.getName());
-        if (fish != null)
-            fish.interact(a -> a.equals("Drop"));
+        final Item shrimp_item = Inventory.getFirst(fish_type.getName());
+        final Item anchovies_item = Inventory.getFirst("Raw anchovies");
+        if (Inventory.containsOnly(fish_type.getRequiredEquipmentIDs()))
+            temp_bool = false;
+
+        if ((shrimp_item != null || anchovies_item != null) && (temp_bool || Inventory.isFull())) {
+            temp_bool = true;
+
+            if (shrimp_item != null)
+                shrimp_item.interact(a -> a.equals("Drop"));
+
+            if (anchovies_item != null)
+                anchovies_item.interact(a -> a.equals("Drop"));
+            return null;
+        }
 
         if (!Inventory.contains(fish_type.getRequiredEquipmentIDs())) {
             for (int id : fish_type.getRequiredEquipmentIDs()) {
